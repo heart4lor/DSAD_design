d64702c9e266bab719232db9be60b5e39fbe0421
syf <syf-98@hotmail.com>
2018-02-26 13:00:42 +0800

    wrote stack part

--- main.cpp
+++ main.cpp
@@ -1,19 +1,81 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 const int MAXLEN = 100;
+
+union Data
+{
+    double numbers;
+    char operators;
+};
+
+struct SeqStack
+{
+    Data data[MAXLEN];
+    int top;
+};
+
+SeqStack* setStack()
+{
+    SeqStack* s = (SeqStack*)malloc(sizeof(SeqStack));
+    s->top = -1;
+    return s;
+}
+
+bool isEmpty(SeqStack* s)
+{
+    return s->top == -1;
+}
+
+bool isFull(SeqStack* s)
+{
+    return s->top == MAXLEN-1;
+}
+
+Data getTop(SeqStack* s)
+{
+    if(!isFull(s) && !isEmpty(s))
+        return s->data[s->top];
+    else
+        printf("Error: Stack full or empty\n");
+}
+
+void push(SeqStack* s, Data x)
+{
+    if(!isFull(s))
+        s->data[++s->top] = x;
+    else
+        printf("Error: Stack is full\n");
+}
+
+void pop(SeqStack* s)
+{
+    if(!isEmpty(s))
+        s->top--;
+    else
+        printf("Error: Stack is empty\n");
+}
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
+    while(true)
+    {
+        double num;
+        char oper;
+        while(scanf("%lf", &num) == 1)
+        {
+            if(scanf("%c", &oper) == 1)
+            {
+                cout << num << oper;
+                if(oper == '=')
+                {
+                    // printf("end.");
+                    SeqStack operands = setStack();
+                    SeqStack operators = setStack();
+                }
+            }
+        }
+    }
 
     return 0;
 }
-
-// 2.算术表达式的求解（限1 人完成）
-// 问题描述：给定一个算术表达式，通过程序求出最后的结果。
-// 基本要求：
-// 1．	从键盘输入要求解的算术表达式；
-// 2．	采用栈结构进行算术表达式的求解过程；
-// 3．	能够判断算术表达式正确与否；
-// 4．	对于错误表达式给出提示；
-// 5．	对于正确的表达式给出最后的结果；
